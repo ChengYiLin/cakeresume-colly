760d4ff16c7313a89a924a458586c53affb8810b
Cheng Yi Lin <cheng.yi.lin.217@gmail.com>
2021-12-27 01:34:00 +0800

    feat : Collect all the job detail URL from Search Page
    TODO: fix error - Too many request

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 
-	urlUtility "github.com/ChengYiLin/cakeresume-colly/utility"
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
+
+type SalaryData struct {
+	Title     string
+	Company   string
+	Link      string
+	MinSalary int
+	MaxSalary int
+	Currency  string
+}
 
 func getSalaryFromText(salaryText string, timeUnit string) int {
 	moneyUnit := string(salaryText[len(salaryText)-1])
@@ -35,72 +44,121 @@ func getSalaryFromText(salaryText string, timeUnit string) int {
 }
 
 func main() {
-	// pageURL := "https://www.cakeresume.com/companies/funpodium/jobs/web-developer-react-web-engineer-react"
-	// pageURL := "https://www.cakeresume.com/companies/ailabs/jobs/sr-front-end-full-stack-engineer"
-	pageURL := "https://www.cakeresume.com/companies/meettheone/jobs/growth-marketing-manager-2eecee"
-	languageParameter := urlUtility.KeyValueParameter("locale", "en")
-	visitURL := urlUtility.AppendQueryString(pageURL, languageParameter)
+	var searchPageURlBase string = "https://www.cakeresume.com/jobs?ref=navs_jobs&refinementList[profession][0]=it_front-end-engineer&refinementList[job_type][0]=full_time&page="
+	var pageNum int = 1
+	var isUrlCollectionFinished bool = false
+
+	// Setup CSV File
+	csvData, _ := os.Create("urlCollector.csv")
+	csvWriter := csv.NewWriter(csvData)
+	ColumnNameList := [][]string{
+		{"Title", "Company", "Link", "MinSalary", "MaxSalary", "Currency"},
+	}
+	csvWriter.WriteAll(ColumnNameList)
 
 	// Create Colly Collector For CakeResume
-	c := colly.NewCollector(
+	urlCollector := colly.NewCollector(
 		colly.AllowedDomains("www.cakeresume.com"),
 	)
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Printf("------------------\nVisiting %s\n------------------\n", r.URL.String())
+	urlCollector.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting %s\n------------------\n", r.URL.String())
 	})
 
-	c.OnHTML(".job-meta-section", func(h *colly.HTMLElement) {
-		var timeUnit string
-		var currency string
-		var minSalary int
-		var maxSalary int
+	// Check Search Result is null
+	urlCollector.OnHTML(".jobs-search-container .no-result", func(h *colly.HTMLElement) {
+		fmt.Println("No Result, Time To Break")
+		isUrlCollectionFinished = true
+	})
 
-		// Step 0. 取得頁面元素
-		salaryText := h.DOM.Find(".job-salary").Text()
-		if len(salaryText) == 0 {
-			fmt.Println("No Salary Data")
-			return
-		}
+	// Get Job URL
+	urlCollector.OnHTML(".job-list-item-content", func(h *colly.HTMLElement) {
+		var recordData SalaryData
+		jobCollector := urlCollector.Clone()
 
-		// Step 1. 取得時間單位
-		textForTimeUnit := strings.Split(salaryText, "/")
-		timeUnit = textForTimeUnit[1]
+		jobTitleDOM := h.DOM.Find(".job-link")
+		jobCompanyDOM := h.DOM.Find(".page-name a")
 
-		// Step 2. 取得 Currency
-		textForCurrency := strings.Split(textForTimeUnit[0], " ")
-		currency = textForCurrency[len(textForCurrency)-1]
+		recordData.Link, _ = jobTitleDOM.Attr("href")
+		recordData.Title = jobTitleDOM.Text()
+		recordData.Company = jobCompanyDOM.Text()
 
-		// Step 3. 取得 最高 及 最低 月薪
-		if strings.Contains(textForCurrency[0], "+") {
-			minSalaryText := strings.ReplaceAll(textForCurrency[0], "+", "")
+		// Web Scrape Job Detail
+		jobCollector.OnHTML(".job-meta-section", func(h *colly.HTMLElement) {
+			// 	// Step 0. 取得頁面元素
+			salaryText := h.DOM.Find(".job-salary").Text()
+			if len(salaryText) == 0 {
+				recordData.MinSalary = 0
+				recordData.MaxSalary = 0
+				recordData.Currency = "TWD"
+				return
+			}
 
-			minSalary = getSalaryFromText(minSalaryText, timeUnit)
-			maxSalary = 0
-		} else {
-			minSalaryText := textForCurrency[0]
-			maxSalaryText := textForCurrency[2]
+			// Step 1. 取得時間單位
+			textForTimeUnit := strings.Split(salaryText, "/")
+			timeUnit := textForTimeUnit[1]
 
-			minSalary = getSalaryFromText(minSalaryText, timeUnit)
-			maxSalary = getSalaryFromText(maxSalaryText, timeUnit)
-		}
+			// Step 2. 取得 Currency
+			textForCurrency := strings.Split(textForTimeUnit[0], " ")
+			recordData.Currency = textForCurrency[len(textForCurrency)-1]
 
-		fmt.Println("== Salary ==")
-		fmt.Printf("Time Unit  : %s\n", timeUnit)
-		fmt.Printf("Currency   : %s\n", currency)
-		fmt.Printf("Min Salary : %d\n", minSalary)
-		fmt.Printf("Max Salary : %d\n", maxSalary)
-		fmt.Println("------------------")
+			// Step 3. 取得 最高 及 最低 月薪
+			if strings.Contains(textForCurrency[0], "+") {
+				minSalaryText := strings.ReplaceAll(textForCurrency[0], "+", "")
 
-		// 技能 tag
-		h.DOM.Find(".labels .label").Each(func(i int, s *goquery.Selection) {
-			// fmt.Println(s.Text())
+				recordData.MinSalary = getSalaryFromText(minSalaryText, timeUnit)
+				recordData.MaxSalary = 0
+			} else {
+				minSalaryText := textForCurrency[0]
+				maxSalaryText := textForCurrency[2]
+
+				recordData.MinSalary = getSalaryFromText(minSalaryText, timeUnit)
+				recordData.MaxSalary = getSalaryFromText(maxSalaryText, timeUnit)
+			}
 		})
+
+		jobCollector.Visit(recordData.Link)
+
+		fmt.Println("Write")
+		csvWriter.Write([]string{
+			recordData.Title,
+			recordData.Company,
+			recordData.Link,
+			strconv.Itoa(recordData.MinSalary),
+			strconv.Itoa(recordData.MaxSalary),
+			recordData.Currency,
+		})
+		csvWriter.Flush()
 	})
 
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Println("Request URL : ", r.Request.URL, "\nfailed with response : ", r, "\nError : ", e)
+	urlCollector.Limit(&colly.LimitRule{
+		DomainGlob: "*cakeresume.*",
+		// Delay:      1 * time.Second,
 	})
 
-	c.Visit(visitURL)
+	urlCollector.OnError(func(r *colly.Response, e error) {
+		isUrlCollectionFinished = true
+		fmt.Println("Request URL : ", r.Request.URL, "\nError : ", e)
+	})
+
+	for {
+		if pageNum == 4 {
+			break
+		}
+
+		if isUrlCollectionFinished {
+			break
+		}
+		urlCollector.Visit(searchPageURlBase + strconv.Itoa(pageNum))
+		pageNum++
+	}
+
+	urlCollector.Wait()
+	defer csvData.Close()
+
+	// 	// 技能 tag
+	// 	h.DOM.Find(".labels .label").Each(func(i int, s *goquery.Selection) {
+	// 		// fmt.Println(s.Text())
+	// 	})
+	// })
 }
